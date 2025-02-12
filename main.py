0b27f2041dfedaeabb77325d298beb829c74777f
Prikazchikov Egor <im3llf@gmail.com>
2025-02-12 18:46:16 +0100

    made changes to main but its not working

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 with open('sneakers - Nike_shoes_2023-04-16.csv', 'r') as forrasfajl:
     for sor in forrasfajl:
         adatok = sor.strip().split(',')
-        cipo = {'title': adatok[0], 'color_breif': adatok[1], 'fullPrice': adatok[2], 'currentPrice': adatok[3], 'publish_date': adatok[4]}
+        cipo = list({'title': adatok[0], 'color_breif': adatok[1], 'fullPrice': map(int, adatok[2]), 'currentPrice': map(int, adatok[3]), 'publish_date': map(int, adatok[4])})
+        # sorrend = lambda title, color, full_price, current_price, publish_date: adatok[0], adatok[1], adatok[2], adatok[3], adatok[4]
 
-    print(f'Valassz, melyik szempont alapjan rendezzem a cipoket?\n1 - title\n2 - color\n3 - full price\n4 - current price\n5 - publish date\n')
-    valasz = int(input('Add meg a lehetoseg szamat! '))
+
+        print(f'Valassz, melyik szempont alapjan rendezzem a cipoket?\n1 - title\n2 - color\n3 - full price\n4 - current price\n5 - publish date\n')
+        valasz = int(input('Add meg a lehetoseg szamat! '))
+        print(sorted(forrasfajl, key=lambda sorrend: adatok[valasz - 1]))
+
+# sorted(cipo[valasz - 1])
